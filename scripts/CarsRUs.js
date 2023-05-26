33366e28c6a23640e51fadccfb38688145313f2e
Rhett Muse <rhett.muse123@gmail.com>
2023-05-26 15:54:38 -0500

    small

--- scripts/CarsRUs.js
+++ scripts/CarsRUs.js
@@ -4,7 +4,7 @@ import { Orders } from './Orders.js'
 export const CarsRUs = () => {
     return `
     <section class="container builder">
-        <h1> Cars 'R Us: Personal Car Builder</h1>
+        <h1>Cars 'R Us: Personal Car Builder</h1>
         ${Builder()}
     </section>
     <section class="container orders">
